c8b58edeb41e98713d8185bc6e2a07e41f611ebf
scozirge <scozirge2@gmail.com>
2023-08-14 18:04:17 +0800

    LOL怪物模型,貼圖,動作匯入 怪物表格編輯

--- HeroFishing/Assets/Scripts/BattleScene/Role/MonsterSpecialize.cs
+++ HeroFishing/Assets/Scripts/BattleScene/Role/MonsterSpecialize.cs
@@ -11,20 +11,24 @@ namespace HeroFishing.Battle {
             if (DieDissolveParticle != null) DieDissolveParticle.gameObject.SetActive(false);
         }
         public void PlayDissolveEffect(SkinnedMeshRenderer _renderer) {
-            if (_renderer.material != null && DieDissolveParticle != null) {
-                if (DieDissolveParticle != null) DieDissolveParticle.gameObject.SetActive(true);
-                //將本來material更改為死亡material並改變texture
-                var tex = _renderer.material.GetTexture("_MainTex");
-                _renderer.material = ResourcePreSetter.GetMaterial("MonsterDie");
-                _renderer.material.SetTexture("_MainTex", tex);
-                //設定material的shader參數
-                var material = _renderer.material;
-                material.SetFloat("_Progress", 0);
-                UniTaskManager.StartTask(GetInstanceID().ToString(), () => {
-                    DOTween.To(() => 0f, x => material.SetFloat("_Progress", x), 1f, GameSettingData.GetFloat(GameSetting.DieEffect_DissolveDecaySec));
-                }, 500);
+            Debug.Log(_renderer.materials.Length);
+            if (DieDissolveParticle != null) DieDissolveParticle.gameObject.SetActive(true);
+            for (int i = 0; i < _renderer.materials.Length; i++) {
+                if (_renderer.materials[i] != null) {
+                    //將本來material更改為死亡material並改變texture
+                    var tex = _renderer.materials[i].GetTexture("_MainTex");
+                    _renderer.materials[i] = ResourcePreSetter.GetMaterial("MonsterDie");
+                    _renderer.materials[i].SetTexture("_MainTex", tex);
+                    //設定material的shader參數
+                    _renderer.materials[i].SetFloat("_Progress", 0);
+                    int index = i;
+                    UniTaskManager.StartTask(GetInstanceID() + "_" + index.ToString(), () => {
+                        DOTween.To(() => 0f, x => _renderer.materials[index].SetFloat("_Progress", x), 1f, GameSettingData.GetFloat(GameSetting.DieEffect_DissolveDecaySec));
+                    }, 500);
 
+                }
             }
+
         }
     }
 }
