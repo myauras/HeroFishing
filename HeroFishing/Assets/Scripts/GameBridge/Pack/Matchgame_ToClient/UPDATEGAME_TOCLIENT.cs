a6fe22b8381ce71ebfb98e447b9a1a30de852903
scoz <scozirge2@gmail.com>
2023-12-10 03:52:08 +0800

    增加封包資料

--- HeroFishing/Assets/Scripts/GameBridge/Pack/Matchgame_ToClient/UPDATEGAME_TOCLIENT.cs
+++ HeroFishing/Assets/Scripts/GameBridge/Pack/Matchgame_ToClient/UPDATEGAME_TOCLIENT.cs
@@ -3,7 +3,14 @@ using UnityEngine;
 
 namespace HeroFishing.Socket.Matchgame {
     public class UPDATEGAME_TOCLIENT : SocketContent {
+        /// <summary>
+        /// 遊戲開始X秒
+        /// </summary>
         public double GameTime { get; private set; }
+        /// <summary>
+        /// 所有玩家的總獲得點數
+        /// </summary>
+        public long[] PlayerGainPoints { get; private set; } = new long[4];
 
         public UPDATEGAME_TOCLIENT() {
         }
