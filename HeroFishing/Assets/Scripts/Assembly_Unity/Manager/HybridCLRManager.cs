1fd997307931714bb0818f4076ef9cafa81d45da
scozirge <scozirge2@gmail.com>
2024-01-19 17:28:16 +0800

    HybridCLR自動化更新

--- HeroFishing/Assets/Scripts/Assembly_Unity/Manager/HybridCLRManager.cs
+++ HeroFishing/Assets/Scripts/Assembly_Unity/Manager/HybridCLRManager.cs
@@ -1,8 +1,10 @@
 ﻿using Cysharp.Threading.Tasks;
 using HybridCLR;
 using Scoz.Func;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 
@@ -15,8 +17,8 @@ public class HybridCLRManager : MonoBehaviour {
 #if UNITY_EDITOR
         return;
 #endif
-        await LoadMetadataForAOTAssemblies();
         await LoadGameAssembly();
+        await LoadMetadataForAOTAssemblies();
     }
     static async UniTask LoadGameAssembly() {
         WriteLog_UnityAssembly.LogColorFormat("開始載入Game Assembly", WriteLog_UnityAssembly.LogType.HybridCLR);
@@ -28,32 +30,7 @@ public class HybridCLRManager : MonoBehaviour {
     }
     static async UniTask LoadMetadataForAOTAssemblies() {
         WriteLog_UnityAssembly.LogColorFormat("開始補充元數據", WriteLog_UnityAssembly.LogType.HybridCLR);
-        List<string> aotDllList = new List<string>
-        {
-            "Cinemachine.dll",
-            "DOTween.dll",
-            "FlutterUnityIntegration.dll",
-            "LitJson.dll",
-            "Loxodon.Framework.dll",
-            "Realm.dll",
-            "SerializableDictionary.dll",
-            "System.Core.dll",
-            "System.dll",
-            "UniRx.dll",
-            "UniTask.dll",
-            "Unity.Addressables.dll",
-            "Unity.Burst.dll",
-            "Unity.Collections.dll",
-            "Unity.Entities.Hybrid.dll",
-            "Unity.Entities.dll",
-            "Unity.RenderPipelines.Core.Runtime.dll",
-            "Unity.ResourceManager.dll",
-            "Unity.VisualScripting.Core.dll",
-            "UnityEngine.AndroidJNIModule.dll",
-            "UnityEngine.CoreModule.dll",
-            "mscorlib.dll",
-        };
-
+        List<string> aotDllList = GetGameAssemblyAotMetaData();
         foreach (var aotDllName in aotDllList) {
             string path = string.Format("Assets/AddressableAssets/Dlls/{0}.bytes", aotDllName);
             WriteLog_UnityAssembly.LogColorFormat("載入元數據Dll: {0}", WriteLog_UnityAssembly.LogType.Addressable, path);
@@ -65,5 +42,34 @@ public class HybridCLRManager : MonoBehaviour {
 
         }
         WriteLog_UnityAssembly.LogColorFormat("補充元數據完成", WriteLog_UnityAssembly.LogType.HybridCLR);
+    }
+    /// <summary>
+    /// 呼叫GameAssembly取需要下載的元數據清單
+    /// </summary>
+    static List<string> GetGameAssemblyAotMetaData() {
+        Assembly targetAssembly = Assembly.Load("Game");
+        // 取得 AOTMetadata 類別型別
+        Type aotMetadataType = targetAssembly.GetType("AOTMetadata");
+
+        if (aotMetadataType != null) {
+            // 取 AotDllList 屬性資訊
+            PropertyInfo aotDllListProperty = aotMetadataType.GetProperty("AotDllList");
+
+            if (aotDllListProperty != null) {
+                // 取 AotDllList 屬性的值
+                List<string> aotDllList = (List<string>)aotDllListProperty.GetValue(null);
+                foreach (string dllName in aotDllList) {
+                    WriteLog_UnityAssembly.LogColor("需載入元數據:" + dllName, WriteLog_UnityAssembly.LogType.HybridCLR);
+                }
+                return aotDllList;
+            } else {
+                WriteLog_UnityAssembly.LogError("抓不到AotDllList 屬性, 有可能Game Assembly的AOTMetadata名稱或命名空間有人改到");
+            }
+        } else {
+            WriteLog_UnityAssembly.LogError("抓不到AotDllList 屬性, 有可能Game Assembly的AOTMetadata名稱或命名空間有人改到");
+        }
+
+        return null;
+
     }
 }
