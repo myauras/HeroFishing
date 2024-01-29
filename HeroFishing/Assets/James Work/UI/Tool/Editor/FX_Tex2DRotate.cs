2ed19808c7786646f964029ce1ccc9fe3193db5b
dogdog <dogdog791206@yahoo.com.tw>
2024-01-29 13:10:28 +0800

    [美術] 新增排行榜第1名特效，相關資源

---
+++ HeroFishing/Assets/James Work/UI/Tool/Editor/FX_Tex2DRotate.cs
@@ -0,0 +1,349 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class FX_Tex2DRotate : Editor
+{
+    static int isgeshi(string path) 
+    {
+        string[] split = path.Split('.');
+        int size = split.Length -1;
+        int zhenjia = 0;
+        if (split[size] == "jpg" || split[size] == "JPG")
+        {
+            zhenjia = 0;
+        }
+        else if (split[size] == "png" || split[size] == "PNG")
+        {
+            zhenjia = 1;
+        }
+        else if (split[size] == "TGA" || split[size] == "tga")
+        {
+            zhenjia = 2;
+        }
+        else
+        {
+            zhenjia = 1;//如果沒有合適的就輸出Png
+        }
+        return zhenjia;
+    }
+    [MenuItem("Assets/FX_貼圖工具箱/貼圖旋轉/順時針90度", true)]
+    private static bool Zjs()
+    {
+        if (Selection.objects[0] as Texture2D || Selection.objects.Length > 1)
+            return true;
+        else return false;
+    }
+    [MenuItem("Assets/FX_貼圖工具箱/貼圖旋轉/順時針90度")]
+    static void Zhengjiushi()
+    {
+        Texture2D yuantu = Selection.objects[0] as Texture2D;
+        string path1 = AssetDatabase.GetAssetPath(yuantu);
+        TextureImporter tex = TextureImporter.GetAtPath(path1) as TextureImporter;
+        bool dx = false;
+        int geshi;
+        geshi = isgeshi(path1);
+        if (tex.isReadable == false)
+        {
+            tex.isReadable = true;
+            AssetDatabase.ImportAsset(path1);
+            dx = true;
+        }
+        Color[] endcolor = new Color[(int)(yuantu.height * yuantu.width)];
+        Texture2D rotateTex = new Texture2D(yuantu.height, yuantu.width, TextureFormat.RGBAFloat, false);
+        int cishu = 0;
+        for (int i = 0; i < yuantu.width; i++)
+        {
+            for (int j = 0; j < yuantu.height; j++)
+            {
+                endcolor[cishu] = yuantu.GetPixel((yuantu.width - i) - 1, j);
+                cishu += 1;
+            }
+        }
+        rotateTex.SetPixels(endcolor);
+        rotateTex.Apply();
+        Save(endcolor, new Vector2(rotateTex.width, rotateTex.height), yuantu.name, path1, tex,geshi);
+        if (dx)
+        {
+            tex.isReadable = false;
+            AssetDatabase.ImportAsset(path1);
+        }
+        AssetDatabase.ImportAsset(path1);
+    }
+    [MenuItem("Assets/FX_貼圖工具箱/貼圖旋轉/逆時針90度", true)]
+    private static bool fujs()
+    {
+        if (Selection.objects[0] as Texture2D || Selection.objects.Length > 1)
+            return true;
+        else return false;
+    }
+    [MenuItem("Assets/FX_貼圖工具箱/貼圖旋轉/逆時針90度")]
+    static void Fujiushi()
+    {
+        Texture2D yuantu = Selection.objects[0] as Texture2D;
+        string path1 = AssetDatabase.GetAssetPath(yuantu);
+        TextureImporter tex = TextureImporter.GetAtPath(path1) as TextureImporter;
+        bool dx = false;
+        int geshi;
+        geshi = isgeshi(path1);
+        if (tex.isReadable == false)
+        {
+            tex.isReadable = true;
+            AssetDatabase.ImportAsset(path1);
+            dx = true;
+        }
+        Color[] endcolor = new Color[(int)(yuantu.height * yuantu.width)];
+        Texture2D rotateTex = new Texture2D(yuantu.height, yuantu.width, TextureFormat.RGBAFloat, false);
+        int cishu = 0;
+        for (int i = 0; i < yuantu.width; i++)
+        {
+            for (int j = 0; j < yuantu.height; j++)
+            {
+                endcolor[cishu] = yuantu.GetPixel(i, (yuantu.height - j) - 1);
+                cishu += 1;
+            }
+        }
+        rotateTex.SetPixels(endcolor);
+        rotateTex.Apply();
+        Save(endcolor, new Vector2(rotateTex.width, rotateTex.height), yuantu.name, path1, tex, geshi);
+        if (dx)
+        {
+            tex.isReadable = false;
+            AssetDatabase.ImportAsset(path1);
+        }
+        AssetDatabase.ImportAsset(path1);
+    }
+
+    [MenuItem("Assets/FX_貼圖工具箱/貼圖旋轉/180度", true)]
+    private static bool yibaiba()
+    {
+        if (Selection.objects[0] as Texture2D || Selection.objects.Length > 1)
+            return true;
+        else return false;
+    }
+    [MenuItem("Assets/FX_貼圖工具箱/貼圖旋轉/180度")]
+    static void YiBaiBashi()
+    {
+        Texture2D yuantu = Selection.objects[0] as Texture2D;
+        string path1 = AssetDatabase.GetAssetPath(yuantu);
+        TextureImporter tex = TextureImporter.GetAtPath(path1) as TextureImporter;
+        bool dx = false;
+        int geshi;
+        geshi = isgeshi(path1);
+        if (tex.isReadable == false)
+        {
+            tex.isReadable = true;
+            AssetDatabase.ImportAsset(path1);
+            dx = true;
+        }
+        Color[] endcolor = new Color[(int)(yuantu.height * yuantu.width)];
+        Color[] lowcolor = new Color[(int)(yuantu.height * yuantu.width)];
+        lowcolor = yuantu.GetPixels();
+        int maxcishu = yuantu.height * yuantu.width;
+        maxcishu -= 1;
+        Texture2D rotateTex = new Texture2D(yuantu.width, yuantu.height, TextureFormat.RGBAFloat, false);
+        int cishu = 0;
+        for (int i = 0; i < yuantu.width; i++)
+        {
+            for (int j = 0; j < yuantu.height; j++)
+            {
+                endcolor[cishu] = lowcolor[maxcishu];
+                cishu += 1;
+                maxcishu -= 1;
+            }
+        }
+        rotateTex.SetPixels(endcolor);
+        rotateTex.Apply();
+        Save(endcolor, new Vector2(rotateTex.width, rotateTex.height), yuantu.name, path1, tex, geshi);
+        if (dx)
+        {
+            tex.isReadable = false;
+            AssetDatabase.ImportAsset(path1);
+        }
+        AssetDatabase.ImportAsset(path1);
+    }
+    [MenuItem("Assets/FX_貼圖工具箱/貼圖旋轉/橫向翻轉", true)]
+    private static bool shuipingfan()
+    {
+        if (Selection.objects[0] as Texture2D || Selection.objects.Length > 1)
+            return true;
+        else return false;
+    }
+    [MenuItem("Assets/FX_貼圖工具箱/貼圖旋轉/橫向翻轉")]
+    static void ShuiPing()
+    {
+        Texture2D yuantu = Selection.objects[0] as Texture2D;
+        string path1 = AssetDatabase.GetAssetPath(yuantu);
+        TextureImporter tex = TextureImporter.GetAtPath(path1) as TextureImporter;
+        bool dx = false;
+        int geshi;
+        geshi = isgeshi(path1);
+        if (tex.isReadable == false)
+        {
+            tex.isReadable = true;
+            AssetDatabase.ImportAsset(path1);
+            dx = true;
+        }
+        Color[] endcolor = new Color[(int)(yuantu.width * yuantu.height)];
+        Texture2D rotateTex = new Texture2D(yuantu.width, yuantu.height, TextureFormat.RGBAFloat, false);
+        int cishu = 0;
+        for (int j = 0; j < yuantu.height; j++)
+        {
+            for (int i = 0; i < yuantu.width; i++)
+            {
+                endcolor[cishu] = yuantu.GetPixel((yuantu.width-i)-1, j);
+                cishu += 1;
+            }
+        }
+        rotateTex.SetPixels(endcolor);
+        rotateTex.Apply();
+        Save(endcolor, new Vector2(rotateTex.width, rotateTex.height), yuantu.name, path1, tex, geshi);
+        if (dx)
+        {
+            tex.isReadable = false;
+            AssetDatabase.ImportAsset(path1);
+        }
+        AssetDatabase.ImportAsset(path1);
+    }
+    [MenuItem("Assets/FX_貼圖工具箱/貼圖旋轉/垂直翻轉", true)]
+    private static bool su()
+    {
+        if (Selection.objects[0] as Texture2D || Selection.objects.Length > 1)
+            return true;
+        else return false;
+    }
+    [MenuItem("Assets/FX_貼圖工具箱/貼圖旋轉/垂直翻轉")]
+    static void ZhongXiang()
+    {
+        Texture2D yuantu = Selection.objects[0] as Texture2D;
+        string path1 = AssetDatabase.GetAssetPath(yuantu);
+        TextureImporter tex = TextureImporter.GetAtPath(path1) as TextureImporter;
+        bool dx = false;
+        int geshi;
+        geshi = isgeshi(path1);
+        if (tex.isReadable == false)
+        {
+            tex.isReadable = true;
+            AssetDatabase.ImportAsset(path1);
+            dx = true;
+        }
+        Color[] endcolor = new Color[(int)(yuantu.width * yuantu.height)];
+        Texture2D rotateTex = new Texture2D(yuantu.width, yuantu.height, TextureFormat.RGBAFloat, false);
+        int cishu = 0;
+        for (int j = 0; j < yuantu.height; j++)
+        {
+            for (int i = 0; i < yuantu.width; i++)
+            {
+                endcolor[cishu] = yuantu.GetPixel(i, yuantu.height-j-1);
+                cishu += 1;
+            }
+        }
+        rotateTex.SetPixels(endcolor);
+        rotateTex.Apply();
+        Save(endcolor, new Vector2(rotateTex.width, rotateTex.height), yuantu.name, path1, tex, geshi);
+        if (dx)
+        {
+            tex.isReadable = false;
+            AssetDatabase.ImportAsset(path1);
+        }
+        AssetDatabase.ImportAsset(path1);
+    }
+    static void Save(Color[] colors, Vector2 kuangao, string name1, string path1, TextureImporter ttt ,int geshi)
+    {
+        TextureFormat texformat;
+        if (geshi == 0)
+        {
+            texformat = TextureFormat.RGB24;
+        }
+        else if (geshi == 1)
+        {
+            texformat = TextureFormat.RGBAFloat;
+        }
+        else if (geshi == 2)
+        {
+            texformat = TextureFormat.RGBAFloat;
+        }
+        else
+        {
+            texformat = TextureFormat.RGBAFloat;
+        }
+        Texture2D tex = new Texture2D((int)kuangao.x, (int)kuangao.y, texformat, false);
+        tex.SetPixels(colors);
+        tex.Apply();
+        byte[] bytes;
+        string houzhui ;
+        if (geshi == 0) 
+        {
+            bytes = tex.EncodeToJPG(100);
+            houzhui = ".jpg";
+        }
+        else if (geshi == 1) 
+        {
+            bytes = tex.EncodeToPNG();
+            houzhui = ".png";
+        }
+        else if (geshi == 2)
+        {
+            bytes = tex.EncodeToTGA();
+            houzhui = ".tga";
+        }
+        else 
+        {
+            bytes = tex.EncodeToPNG();
+            houzhui = ".png";
+        }
+        string temp1 = Jueduilujing();
+        string temp2 = Jueduilujing1(path1);
+        File.WriteAllBytes((temp1 + temp2) + "/" + name1 + houzhui, bytes);
+        AssetDatabase.Refresh();
+        Debug.Log(string.Format("<color=#4CFFB3>{0}</color>", "成功了！We did it！" + "  本次輸出新文件：" + name1 + houzhui));
+        AssetDatabase.Refresh();
+    }
+    static string Jueduilujing()
+    {
+        string patht = Application.dataPath;
+        string[] split = patht.Split('/');
+        string[] cc = new string[split.Length];
+        string newpath = "";
+        for (int i = 0; i < split.Length; i++)
+        {
+            cc[i] = split[i];
+            if (i != split.Length - 1)
+            {
+                if (i != 0)
+                {
+                    newpath = newpath + "/" + cc[i];
+                }
+                if (i == 0)
+                {
+                    newpath = newpath + cc[i];
+                }
+            }
+        }
+        newpath = newpath + "/";
+        return newpath;
+    }
+    static string Jueduilujing1(string patht)
+    {
+        string[] split = patht.Split('/');
+        string[] cc = new string[split.Length];
+        string newpath = "";
+        for (int i = 0; i < split.Length; i++)
+        {
+            cc[i] = split[i];
+            if (i != split.Length - 1)
+            {
+                if (i != 0)
+                {
+                    newpath = newpath + "/" + cc[i];
+                }
+                if (i == 0)
+                {
+                    newpath = "" + cc[i];
+                }
+            }
+        }
+        return newpath;
+    }
+}
---
+++ HeroFishing/Assets/James Work/UI/Tool/Trail/MoveToTrailUV/Editor/MoveToTrailUVEditor.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(MoveToTrailUV))][CanEditMultipleObjects]
+public class MoveToTrailUVEditor : Editor
+{
+   
+
+    SerializedProperty m_moveObject_sp;
+    SerializedProperty m_shaderPropertyName_sp;
+    SerializedProperty m_shaderPropertyID_sp;
+    SerializedProperty m_materialData_sp;
+
+    private MoveToTrailUV m_mttuv;
+
+    private void OnEnable()
+    {
+        
+        m_moveObject_sp = serializedObject.FindProperty("m_moveObject");
+        m_shaderPropertyName_sp = serializedObject.FindProperty("m_shaderPropertyName");
+        m_shaderPropertyID_sp = serializedObject.FindProperty("m_shaderPropertyID");
+        m_materialData_sp = serializedObject.FindProperty("m_materialData");
+
+        m_mttuv = target as MoveToTrailUV;
+        
+        serializedObject.Update();
+        InitializeEditor();
+        serializedObject.ApplyModifiedProperties();
+    }
+
+    private void OnDisable()
+    {
+        serializedObject.Update(); 
+
+        for (int i = 0; i < m_materialData_sp.arraySize; i++)
+        {
+            SerializedProperty materialDataElement_sp = m_materialData_sp.GetArrayElementAtIndex(i);
+            TrailRenderer trailRenderer = (TrailRenderer)materialDataElement_sp.FindPropertyRelative("m_trailRenderer").objectReferenceValue;
+            if (trailRenderer != null)
+            {
+                Material mat = trailRenderer.sharedMaterial;
+                if (mat != null)
+                {
+                    mat.SetFloat(m_shaderPropertyID_sp.intValue, 0f);
+                }
+            }
+        }
+        
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        int checkTrailRenderertile = CheckTrailRendererTile();
+        if (checkTrailRenderertile != -1)
+        {
+            string message = String.Format(" 請用Tile mode >////< ", checkTrailRenderertile);
+            EditorGUILayout.HelpBox(message, MessageType.Warning);
+        }
+
+        string checkTrailRendererShader = CheckTrailRendererShader();
+        if (checkTrailRendererShader != "")
+        {
+            EditorGUILayout.HelpBox(checkTrailRendererShader, MessageType.Warning);
+        }
+
+        EditorGUI.BeginChangeCheck();
+        {
+      
+            EditorGUILayout.PropertyField(m_moveObject_sp);
+            EditorGUILayout.PropertyField(m_shaderPropertyName_sp);
+            EditorGUILayout.PropertyField(m_materialData_sp);
+        }
+        if (EditorGUI.EndChangeCheck())
+        {
+            Undo.RecordObject(target, "MoveToTrailUV changed");
+            InitializeEditor();
+        }
+
+        for (int i = 0; i < m_materialData_sp.arraySize; i++)
+        {
+            SerializedProperty materialDataElement_sp = m_materialData_sp.GetArrayElementAtIndex(i);
+            SyncElementTiling(materialDataElement_sp); 
+        }
+        serializedObject.ApplyModifiedProperties();
+        
+    }
+
+    private void SyncElementTiling(SerializedProperty materialDataElement_sp) 
+    {
+        TrailRenderer trailRenderer = (TrailRenderer)materialDataElement_sp.FindPropertyRelative("m_trailRenderer").objectReferenceValue;
+        
+        if (trailRenderer != null)
+        {
+            
+            Material mat = trailRenderer.sharedMaterial;
+            if (mat != null)
+            {
+                if (materialDataElement_sp.FindPropertyRelative("m_uvTiling").vector2Value != mat.mainTextureScale)
+                {
+                    
+                    if (false)
+                    {
+                        
+                    }
+                    else
+                    {
+                       
+                        materialDataElement_sp.FindPropertyRelative("m_uvTiling").vector2Value = mat.mainTextureScale;
+                    }
+                }
+            }
+        }
+    }
+
+    
+    private void InitializeEditor()
+    {
+        if (m_materialData_sp.serializedObject.targetObject == null || m_materialData_sp.arraySize == 0)
+            return;
+
+        m_shaderPropertyID_sp.intValue = Shader.PropertyToID(m_shaderPropertyName_sp.stringValue);
+
+        
+        for (int i = 0; i < m_materialData_sp.arraySize; i++)
+        {
+            SerializedProperty materialDataElement_sp = m_materialData_sp.GetArrayElementAtIndex(i);
+            SyncElementTiling(materialDataElement_sp); 
+
+            
+            materialDataElement_sp.FindPropertyRelative("m_move").floatValue = 0f;
+        }
+    }
+
+   
+    private int CheckTrailRendererTile()
+    {
+        if (m_mttuv.m_materialData.Length == 0)
+            return -1;
+
+        for (int i = 0; i < m_mttuv.m_materialData.Length; i++)
+        {
+            if (m_mttuv.m_materialData[i].m_trailRenderer == null)
+                continue;
+            TrailRenderer trailRenderer = (TrailRenderer)m_mttuv.m_materialData[i].m_trailRenderer;
+            if (trailRenderer.textureMode != LineTextureMode.Tile)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private string CheckTrailRendererShader()
+    {
+        if (m_mttuv.m_materialData.Length == 0)
+            return "";
+
+        for (int i = 0; i < m_mttuv.m_materialData.Length; i++)
+        {
+            TrailRenderer trailRenderer = m_mttuv.m_materialData[i].m_trailRenderer;
+            if (trailRenderer == null)
+                continue;
+            Material mat = trailRenderer.sharedMaterial;
+            
+            if (mat == null)
+            {
+                string message = String.Format("有沒放材質球喔", i);
+                return message;
+            }
+            
+            
+        }
+
+        return "";
+    }
+}
