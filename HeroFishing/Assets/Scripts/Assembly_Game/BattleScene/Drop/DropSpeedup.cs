cdcd05b5ca03ca82abcffe6178ba5046210c4832
oscar@auro.gg <oscar@auro.gg>
2024-02-02 15:07:16 +0800

    在紅buff使用時，播放Get特效

--- HeroFishing/Assets/Scripts/Assembly_Game/BattleScene/Drop/DropSpeedup.cs
+++ HeroFishing/Assets/Scripts/Assembly_Game/BattleScene/Drop/DropSpeedup.cs
@@ -14,7 +14,8 @@ public class DropSpeedup : DropSpellBase {
 
     public override float Duration => _duration;
 
-    private const string DROP_BUFF = "OtherEffect/Script_{0}";
+    private const string DROP_BUFF_EFFECT = "OtherEffect/Script_{0}";
+    private const string DROP_GET_EFFCT = "OtherEffect/Script_Get{0}";
 
     public DropSpeedup(DropJsonData data, DropSpellJsonData spellData) : base(data, spellData) {
         _duration = spellData.EffectValue1;
@@ -25,8 +26,12 @@ public class DropSpeedup : DropSpellBase {
     public override bool PlayDrop() {
         _hero.AttackSpeedMultiplier = _speedupMultiplier;
         _dropUI.OnDropPlay(_data.ID, _duration);
-        string path = string.Format(DROP_BUFF, _data.Ref);
+        string path = string.Format(DROP_BUFF_EFFECT, _data.Ref);
         PoolManager.Instance.Pop(path, _hero.transform.position, Quaternion.identity, null, go => _buffGO = go);
+
+        path = string.Format(DROP_GET_EFFCT, _data.Ref);
+        PoolManager.Instance.Pop(path, _hero.transform.position, Quaternion.identity, null);
+
         Observable.Timer(TimeSpan.FromSeconds(_duration)).Subscribe(_ => {
             _hero.AttackSpeedMultiplier = 1;
             if (_buffGO != null) {
