f316ba605ccc937a97cbb483fe879358e07c7358
scozirge <scozirge2@gmail.com>
2024-02-07 11:24:36 +0800

    補技能索引註解

--- HeroFishing/Assets/Scripts/Assembly_Game/GameBridge/Pack/Matchgame/LVUPSPELL.cs
+++ HeroFishing/Assets/Scripts/Assembly_Game/GameBridge/Pack/Matchgame/LVUPSPELL.cs
@@ -1,7 +1,7 @@
 ﻿namespace HeroFishing.Socket.Matchgame {
     public class LVUPSPELL : SocketContent {
         //class名稱就是封包的CMD名稱
-        public int SpellIdx { get; private set; }
+        public int SpellIdx { get; private set; } //英雄技能等級, idx傳1~3(技能1~技能3)
 
         public LVUPSPELL(int _spellIdx) {
             SpellIdx = _spellIdx;
