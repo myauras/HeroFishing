901a09e6923569d21d57008d46a108da10000674
oscar@auro.gg <oscar@auro.gg>
2024-02-19 11:15:17 +0800

    drop技能測試

--- HeroFishing/Assets/Scripts/Assembly_Game/Scoz/GMTools/PressKey.cs
+++ HeroFishing/Assets/Scripts/Assembly_Game/Scoz/GMTools/PressKey.cs
@@ -15,9 +15,11 @@ namespace Scoz.Func {
 
 
             if (Input.GetKeyDown(KeyCode.Q)) {
-                int[] monsterIdxs = new int[1] { 1 };
-                key++;
-                GameConnector.Instance.Hit(key, monsterIdxs, "1_attack");
+                DropManager.Instance.AddDrop(0, 15);
+
+                //int[] monsterIdxs = new int[1] { 1 };
+                //key++;
+                //GameConnector.Instance.Hit(key, monsterIdxs, "1_attack");
             } else if (Input.GetKeyDown(KeyCode.W)) {
                 //GameConnector.Instance.Attack(1, "1_attack", 2);
             } else if (Input.GetKeyDown(KeyCode.E)) {
