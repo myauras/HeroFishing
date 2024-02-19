5505367441dc818391d76d9430cc23d9f17aea1d
oscar@auro.gg <oscar@auro.gg>
2024-02-19 15:26:33 +0800

    DROPSPELL_TOCLIENT添加PlayerIdx欄位

--- HeroFishing/Assets/Scripts/Assembly_Game/GameBridge/Pack/Matchgame_ToClient/DROPSPELL_TOCLIENT.cs
+++ HeroFishing/Assets/Scripts/Assembly_Game/GameBridge/Pack/Matchgame_ToClient/DROPSPELL_TOCLIENT.cs
@@ -6,6 +6,7 @@ namespace HeroFishing.Socket.Matchgame {
         //class名稱就是封包的CMD名稱
 
         public bool Success { get; private set; }
+        public int PlayerIdx { get; private set; }
         public int DropSpellJsonID { get; private set; }
 
         public DROPSPELL_TOCLIENT() {
