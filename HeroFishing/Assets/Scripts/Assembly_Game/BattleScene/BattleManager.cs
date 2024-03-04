bcd5ec6021f57be32507532bed1074a85d17d78c
oscar@auro.gg <oscar@auro.gg>
2024-03-04 15:18:14 +0800

    update battle manager

--- HeroFishing/Assets/Scripts/Assembly_Game/BattleScene/BattleManager.cs
+++ HeroFishing/Assets/Scripts/Assembly_Game/BattleScene/BattleManager.cs
@@ -92,6 +92,8 @@ namespace HeroFishing.Battle {
             }
         }
         public void StartGame() {
+            Debug.Log("start game");
+            OnHeroAdd?.Invoke(AllocatedRoom.Instance.HeroIDs[Index], Index);
             UpdateHeros();
             if (GameConnector.Connected)
                 GameConnector.Instance.UpdateScene();
@@ -100,6 +102,7 @@ namespace HeroFishing.Battle {
                 GameConnector.Instance.UpdateScene();
             });
             PopupUI.HideLoading();
+            Screen.sleepTimeout = SleepTimeout.NeverSleep;
         }
         public void RegisterOnLeaveGameEvent(Action _ac) {
             if (_ac == null) return;
