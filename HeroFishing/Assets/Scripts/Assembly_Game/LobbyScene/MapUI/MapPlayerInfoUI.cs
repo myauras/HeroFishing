f031e75b54354c54a1c9fab7ee47c50cc30845b8
oscar@auro.gg <oscar@auro.gg>
2024-03-08 11:46:03 +0800

    添加金錢UI

---
+++ HeroFishing/Assets/Scripts/Assembly_Game/LobbyScene/MapUI/MapPlayerInfoUI.cs
@@ -0,0 +1,39 @@
+using HeroFishing.Main;
+using Scoz.Func;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MapPlayerInfoUI : BaseUI {
+    [SerializeField]
+    private Text _txtName;
+    [SerializeField]
+    private Text _txtLv;
+    [SerializeField]
+    private TextMeshProUGUI _txtCoin;
+    [SerializeField]
+    private TextMeshProUGUI _txtTime;
+    private int _points;
+    
+    public override void RefreshText() {
+        
+    }
+
+    public override void Init() {
+        base.Init();
+        var dbPlayer = GamePlayer.Instance.GetDBPlayerDoc<DBPlayer>(Service.Realms.DBPlayerCol.player);
+        SetPoints((int)(dbPlayer.Point ?? 0));
+    }
+
+    public void AddPoints(int points) {
+        _points += points;
+        _txtCoin.text = _points.ToString();
+    }
+
+    public void SetPoints(int points) {
+        _points = points;
+        _txtCoin.text = _points.ToString();
+    }
+}
