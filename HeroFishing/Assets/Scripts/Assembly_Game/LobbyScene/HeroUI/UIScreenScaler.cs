1d353d5705df2ae01c8fa24561f0da7d9d0a6ce8
oscar@auro.gg <oscar@auro.gg>
2024-03-08 12:05:21 +0800

    Map背景與前景的縮放修正

--- HeroFishing/Assets/Scripts/Assembly_Game/LobbyScene/HeroUI/UIScreenScaler.cs
+++ HeroFishing/Assets/Scripts/Assembly_Game/LobbyScene/HeroUI/UIScreenScaler.cs
@@ -6,10 +6,16 @@ public class UIScreenScaler : MonoBehaviour {
     [SerializeField]
     private Vector2 _refScreenSize = new Vector2(2026, 936);
     private void Start() {
-        var rectTransform = GetComponent<RectTransform>();
-        var aspectRatio = Screen.width / _refScreenSize.x;
-        var height = Screen.height / aspectRatio;
-        var width = height / rectTransform.rect.height * rectTransform.rect.width;
-        rectTransform.sizeDelta = new Vector2(width, height);
+        Scale();
+    }
+
+    public void Scale() {
+        if ((float)Screen.width / Screen.height < _refScreenSize.x / _refScreenSize.y) {
+            var rectTransform = GetComponent<RectTransform>();
+            var aspectRatio = Screen.width / _refScreenSize.x;
+            var height = Screen.height / aspectRatio;
+            var width = height / rectTransform.rect.height * rectTransform.rect.width;
+            rectTransform.sizeDelta = new Vector2(width, height);
+        }
     }
 }
